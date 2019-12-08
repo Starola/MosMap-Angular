8694374f658fd7280e89db3be7e93dcff534320e
Starola <christ-lu@gmx.de>
2019-12-08 13:40:30 +0100

    fix/removal of TestComponent in modulets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestComponent } from './components/test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
   ],
   imports: [
     BrowserModule,
