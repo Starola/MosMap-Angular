482c79786df95a02e864e67c1061ee445c688cb0
Starola <christ-lu@gmx.de>
2020-01-05 02:29:36 +0100

    feature/changeToHomeCompinentWithLogin

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,10 @@ import { AuthService } from '../_services/auth.service';
 export class LoginComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
@@ -17,6 +21,7 @@ export class LoginComponent implements OnInit {
   login() {
     this.authService.login(this.model).subscribe(next => {
       console.log("Log In erfolgreich");
+      this.router.navigate(['/home']);
     }, error => {
       console.log(error);
       console.log("Login fehlgeschlagen");
