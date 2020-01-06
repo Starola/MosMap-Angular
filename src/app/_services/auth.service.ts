f37142b11789dc1b5f6ad981eb17a6befd84c1dd
Starola <christ-lu@gmx.de>
2020-01-06 15:42:27 +0100

    feature/creationOfEnviromentUrl

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment'
 import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'https://localhost:5001/api/auth/'
+  baseUrl = environment.apiUrl + 'auth/'
   constructor(private http: HttpClient) { }
 
   login(model: any) {
