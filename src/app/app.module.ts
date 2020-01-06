15ef3314398f11fdf80fe617cf1bd0ae448b8ea7
Starola <christ-lu@gmx.de>
2020-01-06 14:52:30 +0100

    feature/footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { RegisterComponent } from './register/register.component';
     NavComponent,
     LoginComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,7 +8,8 @@ import {
   MatCardModule,
   MatInputModule,
   MatFormFieldModule,
-  MatDividerModule
+  MatDividerModule,
+  MatSidenavModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatDividerModule
+    MatDividerModule,
+    MatSidenavModule
   ],
   exports: [
     MatButtonModule,
@@ -30,7 +32,8 @@ import {
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatDividerModule
+    MatDividerModule,
+    MatSidenavModule
   ]
 })
 export class MaterialModule {}
