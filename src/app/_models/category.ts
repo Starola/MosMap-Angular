acaca644c372efb06a61c032e618153fc2b2dfbd
Starola <christ-lu@gmx.de>
2020-01-28 15:57:53 +0100

    feature/ShowCategoryNameInLocationList

--- src/app/_models/category.ts
+++ src/app/_models/category.ts
@@ -1,5 +1,5 @@
 export interface Category {
     id: number;
-    CategoryName: string;
+    categoryName: string;
     CategoryDescription: string;
 }
--- src/app/home/locationList/locationCategoryList/locationCategoryList.component.ts
+++ src/app/home/locationList/locationCategoryList/locationCategoryList.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LocationService } from 'src/app/_services/location.service';
+import { CategoryService } from 'src/app/_services/category.service';
+import { Category } from 'src/app/_models/category';
 
 @Component({
   selector: 'app-locationCategoryList',
@@ -10,18 +12,29 @@ export class LocationCategoryListComponent implements OnInit {
   @Input() categoryId: number;
 
   locationList: Location[];
+  categoryName: string;
 
   constructor(
-    private locationService: LocationService
+    private locationService: LocationService,
+    private categoryService: CategoryService
   ) { }
 
   ngOnInit() {
     this.getLocations();
+    this.getCategoryName();
   }
 
   getLocations() {
     this.locationService.getLocations(this.categoryId).subscribe((locations: Location[]) => {
       this.locationList = locations;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getCategoryName() {
+    this.categoryService.getCategorieById(this.categoryId).subscribe((category: Category) => {
+      this.categoryName = category.categoryName;
     }, error => {
       console.log(error);
     });
